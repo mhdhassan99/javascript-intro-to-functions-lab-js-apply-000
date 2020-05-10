2fe30cba56c12b3200a9395b6d9b1f8e7a622f22
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-10 03:36:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+// shout(string)
+//     1) receives one argument and returns it in all caps
+
+//   whisper(string)
+//     2) receives one argument and returns it in all lowercase
+
+//   logShout(string)
+//     3) calls console.log() its one argument in all caps
+
+//   logWhisper(string)
+//     4) calls console.log() its one argument in all lowercase
+
+//   sayHiToGrandma(string)
+//     5) returns "I can't hear you!" if `string` is lowercase
+//     6) returns "YES INDEED!" if `string` is uppercase
+//     7) returns "I love you, too." if `string` is "I love you, Grandma."`
+
+Function shout(string) {
+  return string.toUpperCase();
+}
