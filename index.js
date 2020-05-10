dc22d9974f8122b7e57f1f391bdc01508726a0fc
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-10 03:49:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    
+    console.log('I can't hear you!') 
   }else if (string === 'I love you, Grandma.') {
     var sh = 'I love you, too.'
     return sh
